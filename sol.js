0a974ce5a20ecb517a32738e439d8e3fdb33a3cc
yashPundhir <yash.pundhir.prof@gmail.com>
2023-05-31 23:50:39 +0530

    Sol 1 & 3 uncommented

--- sol.js
+++ sol.js
@@ -18,24 +18,24 @@ Output0 [0,1]
 
 // Solution 1
 
-// function twoSum(nums, target) {
-// 	let compObj = {};
+function twoSum(nums, target) {
+	let compObj = {};
 
-// 	if (nums.length === 1 || nums.length === 0) {
-// 		return "No Solution Possible";
-// 	} else {
-// 		for (let index = 0; index < nums.length; index++) {
-// 			const complement = (nums[index] - target) * -1;
-// 			if (complement in compObj) {
-// 				return [compObj[complement], index];
-// 			}
-// 			compObj[nums[index]] = index;
-// 		}
-// 		return [];
-// 	}
-// }
+	if (nums.length === 1 || nums.length === 0) {
+		return "No Solution Possible";
+	} else {
+		for (let index = 0; index < nums.length; index++) {
+			const complement = (nums[index] - target) * -1;
+			if (complement in compObj) {
+				return [compObj[complement], index];
+			}
+			compObj[nums[index]] = index;
+		}
+		return [];
+	}
+}
 
-// console.log(twoSum([4, 7, -4, -5, 8, -2], 2));
+console.log(twoSum([4, 7, -4, -5, 8, -2], 2));
 
 /*
 
@@ -69,24 +69,24 @@ Output: 2
 
 // Solution 3
 
-// function find(array, target) {
-// 	let left = 0;
-// 	let right = array.length - 1;
-// 	let mid = 0;
-// 	while (left <= right) {
-// 		mid = Math.floor((left + right) / 2);
-// 		if (target === array[mid]) {
-// 			return `target found at index ${mid}`;
-// 		} else if (target > array[mid]) {
-// 			left = mid + 1;
-// 		} else if (target < array[mid]) {
-// 			right = mid - 1;
-// 		}
-// 	}
-// 	return `target is not found and thus target should be inserted at index ${left}`;
-// }
+function find(array, target) {
+	let left = 0;
+	let right = array.length - 1;
+	let mid = 0;
+	while (left <= right) {
+		mid = Math.floor((left + right) / 2);
+		if (target === array[mid]) {
+			return `target found at index ${mid}`;
+		} else if (target > array[mid]) {
+			left = mid + 1;
+		} else if (target < array[mid]) {
+			right = mid - 1;
+		}
+	}
+	return `target is not found and thus target should be inserted at index ${left}`;
+}
 
-// console.log(find([-5, -2, 0, 3, 5, 7, 9], -1));
+console.log(find([-5, -2, 0, 3, 5, 7, 9], -1));
 
 /*
 
