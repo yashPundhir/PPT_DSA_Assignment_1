35399a8b53b77aa4b80f7e62f2e52c3a30beb2fb
yashPundhir <yash.pundhir.prof@gmail.com>
2023-06-01 00:13:45 +0530

    Ques 6 Solved

--- sol.js
+++ sol.js
@@ -125,3 +125,51 @@ function increment(digits) {
 	return digits;
 }
 console.log(increment([4, 6, 7]));
+
+/*
+
+**Q5.** You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+**Example 1:**
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+
+**Explanation:** The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+*/
+
+/*
+
+**Q6.** Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+**Example 1:**
+Input: nums = [1,2,3,1]
+
+Output: true
+
+*/
+
+// Solution 6
+
+function check(nums) {
+	let hash = {};
+	for (const num of nums) {
+		if (num in hash) {
+			hash[num] = hash[num] + 1;
+		} else {
+			hash[num] = 1;
+		}
+	}
+	for (const key in hash) {
+		if (hash.hasOwnProperty(key)) {
+			if (hash[key] > 1) return true;
+		}
+	}
+	return false;
+}
+console.log(check([3, 5, 1, 4, 6, 2, 3, 5, 2]));
